65a48d146450295f8c8195f01a5ebcd27173ada7
Harun Arman <94543053+HarunArman@users.noreply.github.com>
2023-04-01 00:19:41 +0300

    Update main.R

--- dashboard/api/allocator_output/main.R
+++ dashboard/api/allocator_output/main.R
@@ -2,7 +2,7 @@ library(plumber)
 
 #########INITIAL_PATH_SET############
 readRenviron(".Renviron")
-robyn_api_project_path <- Sys.getenv("ROBYN_API_PROJECT_PATH")
+robyn_api_project_path <- setwd("~/dashboard")
 robyn_api_ip <- Sys.getenv("ROBYN_API_IP")
 robyn_api_port <- as.integer(Sys.getenv("ROBYN_API_PORT"))
 
@@ -12,6 +12,6 @@ print(paste0("roby_api ip is set to: ", robyn_api_ip, " port is: ", robyn_api_po
 #########INITIAL_PATH_SET DONE############
 
 
-r <- plumb(paste0(robyn_api_project_path, "api/allocator_output/simulator_controller.R"))
+r <- plumb(paste0(robyn_api_project_path, "/api/allocator_output/simulator_controller.R"))
 
 r$run(port=robyn_api_port, host=robyn_api_ip)
