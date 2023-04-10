c4250e2caf4239ba16907c60037d6f21e12913c7
Harun Arman <94543053+HarunArman@users.noreply.github.com>
2023-04-10 21:44:53 +0300

    Update allocator.R

--- dashboard/api/allocator_output/allocator.R
+++ dashboard/api/allocator_output/allocator.R
@@ -7,7 +7,7 @@ set.seed(123)
 ## force multicore when using RStudio
 Sys.setenv(R_FUTURE_FORK_ENABLE="true")
 options(future.fork.enable = TRUE)
-robyn_api_project_path <- Sys.getenv("ROBYN_API_PROJECT_PATH")
+robyn_api_project_path <- setwd("~/dashboard")
 
 ### environment path
 robyn_object <- readRDS(paste0(robyn_api_project_path , "api/allocator_output/robyn_object.RDS"))
